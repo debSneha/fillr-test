9b211bb6dec0c83677c3130fe625b34161183452
snehaDebsikdar <snehadebsikdar@gmail.com>
2024-10-25 18:07:05 +1100

    implemented form scraper

--- widget/widget.js
+++ widget/widget.js
@@ -4,15 +4,45 @@
 // which describes all the fields in each frame.
 
 // This is a template to help you get started, feel free to make your own solution.
+
+
+// function to scrape or extract form fields 
+function extractFormFields(){
+  try{
+    const fields = []
+    // get all the form controls 
+    // only input and select is needed for present test 
+    // but can be extended to use other form elements like textarea
+    const controls = document.querySelectorAll('input, select, textarea')
+
+    // loops over all controls and gets the name attribute and label for same
+    controls.forEach(control => { 
+
+      let name = control.getAttribute('name')
+      let labels = control.labels
+      if(labels && labels.length > 0 && name){
+        fields.push({ [name] : labels[0].innerText})
+      }
+
+    });
+    return fields
+  }catch(e){
+    console.log(e)
+  }
+}
+
 function execute() {
 	try {
+  
     // Step 1 Scrape Fields and Create Fields list object.
     // Step 2 Add Listener for Top Frame to Receive Fields.
     if (isTopFrame()) {
+  
       window.addEventListener('message', (event) => {
         // - Merge fields from frames.
         // - Process Fields and send event once all fields are collected.
       });
+
     } else if (!isTopFrame()) {
       // Child frames sends Fields up to Top Frame.
     }
